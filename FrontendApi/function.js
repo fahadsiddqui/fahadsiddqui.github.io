d2eba24b50a55d72f78be038ecf7f25416c004f2
Fahad Siddiqui <fahadsiddqui88@gmail.com>
2021-11-15 16:15:55 +0500

    Update function.js

--- FrontendApi/function.js
+++ FrontendApi/function.js
@@ -4,7 +4,7 @@ let father_name = document.getElementById('father_name').value;
 let age = document.getElementById('age').value;
 let roll_no = document.getElementById('roll_no').value;
 
-axios.post('http://pacific-coast-71393.herokuapp.com/user',{
+axios.post('https://pacific-coast-71393.herokuapp.com/user',{
     student_name:student_name, father_name:father_name,age:age,roll_no:roll_no
 }).then(function(response){
     console.log(response);
@@ -15,7 +15,7 @@ axios.post('http://pacific-coast-71393.herokuapp.com/user',{
 }
 
 function get_all(){
-axios.get('http://pacific-coast-71393.herokuapp.com/users')
+axios.get('https://pacific-coast-71393.herokuapp.com/users')
 .then(function(response){
     console.log(response);
     $html ='';
@@ -61,7 +61,7 @@ function update_student(){
     let roll_no = document.getElementById('roll_no').value;
     let id = document.getElementById('student_id').value;
 
-axios.put('http://pacific-coast-71393.herokuapp.com/user/'+id,{
+axios.put('https://pacific-coast-71393.herokuapp.com/user/'+id,{
     student_name:student_name, father_name:father_name,age:age,roll_no:roll_no
 }).then(response => {
     console.log(response);
@@ -74,7 +74,7 @@ axios.put('http://pacific-coast-71393.herokuapp.com/user/'+id,{
 
 function delete_student(){
     let id = document.getElementById('student_id').value;
-axios.delete('http://pacific-coast-71393.herokuapp.com/user/'+id)
+axios.delete('https://pacific-coast-71393.herokuapp.com/user/'+id)
 .then(function(response) {
     console.log(response);
     alert("record deleted");
